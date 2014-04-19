7d8258bbd609d1f013f16d80e7f76e5fdf68dc80
Nick Brown <nick@nickbrown.me>
2014-04-19 14:36:27 -0700

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+require.config({
+    shim: {
+        underscore: {
+            exports: '_',
+        },
+
+        backbone: {
+            deps: ['underscore', 'jquery']
+        },
+
+        marionette: {
+            deps: ['backbone'],
+            exports: 'Backbone.Marionette'
+        }
+    },
+
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        underscore: '../bower_components/underscore/underscore',
+        backbone: '../bower_components/backbone/backbone',
+        marionette: '../bower_components/marionette/lib/backbone.marionette',
+        mustache: '../bower_components/mustache/mustache'
+    }
+});
+require(['app', 'marionette'], function(App, Marionette) {
+    'use strict';
+
+    App.start();
+});
